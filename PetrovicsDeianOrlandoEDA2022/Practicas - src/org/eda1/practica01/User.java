958f16edcc29fd36cd2d35082a1445108dc39fc2
BuildTools <unconfigured@null.spigotmc.org>
2021-09-14 17:53:47 +0200

    Subida desde el portatil

--- PetrovicsDeianOrlandoEDA2022/Practicas - src/org/eda1/practica01/User.java
+++ PetrovicsDeianOrlandoEDA2022/Practicas - src/org/eda1/practica01/User.java
@@ -4,10 +4,10 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Userr {
+public class User {
 	private String name;
 	private ArrayList<Device> devices;
-	
+
 	public User(String name) {
 		//Si name == null se lanza excepcion de tipo RuntimeException()
 		//3 lineas
@@ -15,31 +15,31 @@ public class Userr {
 		this.name = name.trim();
 		this.devices = new ArrayList<Device>();
 	}
-	
+
 	public void clear() {
 		//1 for()
 		//...
-		
+
 		for (Device dev : devices) {
 			dev.clear();
 		}
-		
+
 		devices.clear();
 	}
-	
+
 	public boolean addDevices(Device... devs) {
 		if (devs == null) return false;
 		//1 for()
 		//...
-		
-		for (Device dev : devs) {
-			if(this.devices.contains(dev)) continue;
-			this.devices.add(dev);
-		}
-		
+
+		//		for (Device dev : devs) {
+		//			if(this.devices.contains(dev)) continue;
+		//			this.devices.add(dev);
+		//		}
+		//		
 		return true;
 	}
-	
+
 	public int getNumDevices() {
 		return this.devices.size();
 	}
@@ -47,8 +47,8 @@ public class Userr {
 	public boolean loadMessages(Device dev, String fileName) {
 		//Analizar la estructura de este tipico metodo de carga de datos a partir de un archivo de texto
 		//Por que es necesario el bloque try{}catch(){}
-		
-		
+
+
 		Scanner scan = null;
 		String line; 
 		int pos = this.devices.indexOf(dev);
@@ -65,41 +65,41 @@ public class Userr {
 		scan.close();
 		return true;
 	}
-	
+
 	public boolean sendMessage(Device dev, String msg) {
 		int pos = this.devices.indexOf(dev);
 		//2 lineas
 		//...
 		return true;
 	}
-	
+
 	public void substitute(String word1, String word2) {
 		//Sustituimos en todos los dispositivos...
 		//1 for()
 	}
-	
+
 	public boolean contains(String word) {
 		//Buscamos en todos y cada uno de los dispositivos
 		//1 for()
 		//...
 		return false;
 	}
-	
+
 	public String getWords() {
 		String result = "";
 		//1 for()
 		//...
 		return result;
 	}
-	
+
 	public ArrayList<String> getOrderedWords() {
 		ArrayList<String> result = new ArrayList<String>();
 		//1 for()
-		
+
 		result.sort(null); //que metodo de ordenacion estamos utilizando aqui...
 		return result;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.name;
